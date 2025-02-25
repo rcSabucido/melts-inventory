ce45a88cbab7d06e053097fb7fe80614750a26d3
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-02-25 16:33:49 +0800

    Add first table template to Restock.jsx

--- src/pages/Restock.jsx
+++ src/pages/Restock.jsx
@@ -18,8 +18,84 @@ const RestockPage = () => {
               Add Stock
               </Button>
           </div>
-          <div className='mx-7 w-auto h-80 bg-amber-200/30 rounded-xl'>
-            <p className='mx-3 font-medium'>Month-Day-Year</p>
+          <div className='mx-7 w-auto h-70 bg-amber-200/30 rounded-xl'>
+            <p className='px-4 pt-4 text-xl font-bold'>February 20, 2025</p>
+            <div class="relative overflow-x-auto rounded-xl m-5">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-orange-200/70">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Product
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Added Items
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Supplier
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Expiry Date
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-amber-100 border-b border-gray-200 text-gray-900">
+                            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                              Adam Smasher
+                            </th>
+                            <td class="px-6 py-4">
+                                Drinks
+                            </td>
+                            <td class="px-6 py-4">
+                                13
+                            </td>
+                            <td class="px-6 py-4">
+                                Melts Inc.
+                            </td>
+                            <td class="px-6 py-4">
+                                2026-02-19
+                            </td>
+                        </tr>
+                        <tr class="bg-amber-100 border-b  border-gray-200 text-gray-900">
+                            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                              Biryani
+                            </th>
+                            <td class="px-6 py-4">
+                                Food
+                            </td>
+                            <td class="px-6 py-4">
+                              20
+                            </td>
+                            <td class="px-6 py-4">
+                                Melts Inc.
+                            </td>
+                            <td class="px-6 py-4">
+                                2026-02-19
+                            </td>
+                        </tr>
+                        <tr class="bg-amber-100 text-gray-900">
+                            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                                David Martinez
+                            </th>
+                            <td class="px-6 py-4">
+                                Drinks
+                            </td>
+                            <td class="px-6 py-4">
+                                19
+                            </td>
+                            <td class="px-6 py-4">
+                                Melts Inc.
+                            </td>
+                            <td class="px-6 py-4">
+                                2026-02-19
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
           </div>
         </main>
       </div>
