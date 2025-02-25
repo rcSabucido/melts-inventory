1ba402f369a4c7beb6b0acf86d2738944242f3fe
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-02-25 15:43:36 +0800

    Add initial div to store table contents

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
     return (
-        <button type='button' className="flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+        <button type='button' className="flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
             {children}
         </button>
     );
--- src/pages/Restock.jsx
+++ src/pages/Restock.jsx
@@ -8,8 +8,8 @@ const RestockPage = () => {
     <>
       <div className="flex">
         <Sidebar />
-        <main className="relative p-4 bg-amber-100 w-full">
-          <div className='absolute top-0 right-0 mx-13 my-6 flex gap-2'>
+        <main className="flex-col p-4 bg-amber-100 w-full">
+          <div className='mx-5 my-3 flex justify-end gap-2'>
             <Button>
               <FunnelIcon className='h-6 w-6' />
               </Button>
@@ -17,6 +17,9 @@ const RestockPage = () => {
               <PlusIcon className='h-6 w-6'/>
               Add Stock
               </Button>
+          </div>
+          <div className='mx-7 w-auto h-80 bg-amber-200/30 rounded-xl'>
+            <p className='mx-3 font-medium'>Month-Day-Year</p>
           </div>
         </main>
       </div>
