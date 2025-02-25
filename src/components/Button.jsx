48f0e46bbb86f1b8dbad2e56e873fc1367d138be
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-02-25 14:52:00 +0800

    Add top-right buttons to Restock.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
     return (
-        <button type='button' className="text-white bg-orange-400/70 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+        <button type='button' className="flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
             {children}
         </button>
     );
--- src/pages/Restock.jsx
+++ src/pages/Restock.jsx
@@ -1,5 +1,6 @@
 import Button from '../components/Button.jsx';
 import Sidebar from '../components/Sidebar.jsx';
+import { FunnelIcon, PlusIcon } from '@heroicons/react/24/solid';
 
 
 const RestockPage = () => {
@@ -9,8 +10,13 @@ const RestockPage = () => {
         <Sidebar />
         <main className="relative p-4 bg-amber-100 w-full">
           <div className='absolute top-0 right-0 mx-13 my-6 flex gap-2'>
-            <Button>bob</Button>
-            <Button>phil</Button>
+            <Button>
+              <FunnelIcon className='h-6 w-6' />
+              </Button>
+            <Button>
+              <PlusIcon className='h-6 w-6'/>
+              Add Stock
+              </Button>
           </div>
         </main>
       </div>
