d2d8e037f01d8c9c033cd55eb7ed12d44f759255
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-02-25 14:37:21 +0800

    Fix logic in Button component and import buttons in Restock.js

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ content }) => {
+const Button = ({ children }) => {
     return (
         <button type='button' className="text-white bg-orange-400/70 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
-            {content}
+            {children}
         </button>
     );
 }
--- src/pages/Restock.jsx
+++ src/pages/Restock.jsx
@@ -1,3 +1,4 @@
+import Button from '../components/Button.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 
 
@@ -8,7 +9,8 @@ const RestockPage = () => {
         <Sidebar />
         <main className="relative p-4 bg-amber-100 w-full">
           <div className='absolute top-0 right-0 mx-13 my-6 flex gap-2'>
-            
+            <Button>bob</Button>
+            <Button>phil</Button>
           </div>
         </main>
       </div>
