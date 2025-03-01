819f979c173c3a45ee711658c6f497e5dd213ccb
LegionRevenant <danbap300@gmail.com>
2025-03-01 18:11:04 +0800

    Creating NearExpiry and SearchBar components

---
+++ src/components/NearExpiry.jsx
@@ -0,0 +1,59 @@
+
+const NearExpiry  = () => {
+    return (
+      <div class="border-solid border-orange-400/40 border-2 rounded-md w-full">
+      <table class="table-auto text-left rtl:text-right w-full">
+        <thead class="text-gray-600 bg-orange-300/30">
+          <tr>
+            <th class="px-6 py-3">
+              Product
+            </th>
+            <th class="px-6 py-3">
+              Items
+            </th>
+            <th class="px-6 py-3">
+              Category
+            </th>
+            <th class="px-6 py-3">
+              Days Left
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+          </tr>
+          <tr>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+          </tr>
+          <tr>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Banana Split</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">12</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+          </tr>
+          <tr>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Lorem Liquorice</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">15</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+          </tr>
+          <tr>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Ipsum Candy</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">25</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</th>
+            <th class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</th>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    )
+  }
+  
+  export default NearExpiry;
