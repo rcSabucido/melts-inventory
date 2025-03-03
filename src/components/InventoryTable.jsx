653f382d86deeee535c51dbce441eedc39d27857
LegionRevenant <danbap300@gmail.com>
2025-03-03 16:18:55 +0800

    '#7 Add comment for inset-y[]'

--- src/components/InventoryTable.jsx
+++ src/components/InventoryTable.jsx
@@ -1,5 +1,3 @@
-import { PencilSquareIcon,TrashIcon } from '@heroicons/react/24/solid';
-
 
 const InventoryTable = () => {
     return (
@@ -26,11 +24,44 @@ const InventoryTable = () => {
         <tbody>
           <tr>
             <div>
-            <TrashIcon 
-            fill=''
-            strokeWidth={1.5}
-            class="h-6 absolute inset-y-65 left-0 w-208"/>
-            <PencilSquareIcon class="h-6 absolute inset-y-65 left-0 w-180" />
+            {/* inset-y-[+16]*/}
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none"
+            stroke="#000000" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+            class="absolute inset-y-65 left-0 w-208">
+            <path d="M3 6h18">
+            </path>
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6">
+            </path>
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2">
+                </path>
+            </svg>
+            
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="#000000" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+            class="absolute inset-y-65 left-0 w-180">
+            <path 
+            d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+            </path>
+            <path 
+            d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z">
+            </path>
+            </svg>
             </div>
             <th class="px-6 py-5 border-solid border-t-0">Mango Chip</th>
             <th class="px-6 py-5 border-solid border-t-0">59</th>  
