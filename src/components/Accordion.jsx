28d85ca6f686845879cdc900a9631e10e933c4ed
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-03 15:56:04 +0800

    #3 Add skeletal structure to Accordion component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,13 @@
-const Accordion = ({}) => {
+import { useState } from 'react';
+const Accordion = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div>
-        <h1>Accordion</h1>
+        <div className=''>
+            <div className='' onClick={() => setIsOpen(!isOpen)}>
+                <h3>{title}</h3>
+                <span>{isOpen ? '-' : '+'}</span>
+            </div>
+            {isOpen && <div className=''>{children}</div>}
         </div>
     );
 }
