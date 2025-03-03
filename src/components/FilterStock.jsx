d0d5d0c979e9d1044dd4a9ae0e90efeefc9882e2
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-03 16:00:33 +0800

    #3 Add skeletal structure to FilterStock component

--- src/components/FilterStock.jsx
+++ src/components/FilterStock.jsx
@@ -1,7 +1,25 @@
+import Accordion from "./Accordion";
+
 const FilterStock = () => {
     return (
-        <div></div>
-    )
+        <div className="">
+            <Accordion title='Columns'>
+                <div>
+                    <label><input type='checkbox' /> Product</label>
+                    <label><input type='checkbox' /> Category</label>
+                    <label><input type='checkbox' /> Added Items</label>
+                    <label><input type='checkbox' /> Supplier</label>
+                    <label><input type='checkbox' /> Expiry Date</label>
+                </div>
+            </Accordion>
+            <Accordion title='Added Date'>
+                <input type='date' />
+            </Accordion>
+            <Accordion title='Expiry Date'>
+                <input type='date' />
+            </Accordion>
+        </div>
+    );
 }
 
 export default FilterStock;
