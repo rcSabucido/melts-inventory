fbb1d0303436e66df16260496b47a52468c794f2
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-04 15:22:09 +0800

    #8 Create Add Employee button in Employee page

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,12 +1,20 @@
 import Sidebar from '../components/Sidebar.jsx';
+import Button from '../components/Button.jsx';
+import { PlusIcon } from '@heroicons/react/24/solid';
 
 const EmployeePage = () => {
   return (
     <>
       <div className="flex">
         <Sidebar />
-        <main className="p-4 bg-amber-100 w-full">
-          <h2>This is an employee page!</h2>
+        <main className="p-4 flex-col bg-amber-100 w-full">
+          <div className='flex justify-end mx-5 my-3'>
+            <Button>
+              <PlusIcon className="h-6 w-6" />
+              Add Employee
+            </Button>
+          </div>
+          
         </main>
       </div>
     </>
