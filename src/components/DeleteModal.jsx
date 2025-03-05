fe74671ca67a9b17de54e36a4019f43e7f98c3d3
LegionRevenant <danbap300@gmail.com>
2025-03-05 23:34:05 +0800

    '#7 Create DeleteModal'

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom';
 
 const DeleteModal = ({ onClose }) => {
 
+    
     return createPortal(
         <div className="fixed inset-0 flex justify-center items-center bg-gray-800/40">
             <div className="bg-amber-100 p-6 rounded-lg w-1/4">
