06a52c465c99f393f83c157ebfdc136918be500b
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-06 13:32:19 +0800

    #8 Create AddEmployee page

---
+++ src/pages/AddEmployee.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "../components/Sidebar";
+
+const AddEmployee = () => {
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <main className="p-4 flex-col bg-[#ffffdb] w-full">
+                    <div>test</div>
+                </main>
+            </div>
+        </>
+    );
+}
+
+export default AddEmployee;
