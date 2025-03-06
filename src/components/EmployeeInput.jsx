58eb1005119eb65913898e091c73068f9ce7d6a7
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-06 15:52:27 +0800

    #8 Finish skeletal structure of forms in EmployeeInput

--- src/components/EmployeeInput.jsx
+++ src/components/EmployeeInput.jsx
@@ -52,11 +52,53 @@ const EmployeeInput = () => {
                     </div>
                     <div className="m-4 grow">
                         <label className="block text-sm font-medium text-gray-700">Gender</label>
-                        <select className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md">
+                        <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md">
                             <option value="male">Male</option>
                             <option value='female'>Female</option>
                             <option value='other'>Other</option>
                         </select>
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-between">
+                    <div className="m-4 grow">
+                        <label className="block text-sm font-medium text-gray-700">Province</label>
+                        <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md">
+                            <option value='davao del sur'>Davao Del Sur</option>
+                            <option value='davao de oro'>Davao De Oro</option>
+                        </select>
+                    </div>
+                    <div className="m-4 grow">
+                        <label className="block text-sm font-medium text-gray-700">City</label>
+                        <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md">
+                            <option value='davao city'>Davao City</option>
+                            <option value='tagum city'>Tagum City</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-between">
+                    <div className="m-4 grow">
+                        <label className="block text-sm font-medium text-gray-700">District</label>
+                        <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md">
+                            <option value='first'>1st</option>
+                            <option value='second'>2nd</option>
+                        </select>
+                    </div>
+                    <div className="m-4 grow">
+                        <label className="block text-sm font-medium text-gray-700">Barangay</label>
+                        <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md">
+                            <option value='cabantian'>Cabantian</option>
+                            <option value='indangan'>Indangan</option>
+                            <option value='nabunturan'>Nabunturan</option>
+                        </select>
+                    </div>
+                    <div className="m-4 grow">
+                        <label className="block text-sm font-medium text-gray-700">Street</label>
+                        <input
+                            type='text'
+                            name='street'
+                            className='mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md'
+                            required 
+                        />
                     </div>
                 </div>
             </div>
