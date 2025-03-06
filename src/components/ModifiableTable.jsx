b97a51803ab342ca1a32d0753a63433a4b9cab3e
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-03-06 17:20:46 +0800

    Add delete confirmation modal for suppliers table

--- src/components/ModifiableTable.jsx
+++ src/components/ModifiableTable.jsx
@@ -1,6 +1,6 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 
-const ModifiableTable = ({ columns, data, className }) => {
+const ModifiableTable = ({ columns, data, className, onEditClick, onDeleteClick }) => {
     let addClassName = className;
     return (
       <div className={`relative overflow-x-auto rounded-xl m-5 ${addClassName}`}>
@@ -18,8 +18,8 @@ const ModifiableTable = ({ columns, data, className }) => {
               <tr key={rowIndex} className="bg-[#fff2bf] border-b border-gray-200 text-gray-900">
                 <td key="0" className="w-25 px-5">
                   <div className='flex gap-8 justify-center items-center'>
-                    <PencilIcon className='h-5 w-5'/>
-                    <TrashIcon className='h-5 w-5'/>
+                    <PencilIcon className='h-5 w-5' onClick={() => onEditClick(row)} />
+                    <TrashIcon className='h-5 w-5' onClick={() => onDeleteClick(row)} />
                   </div>
                 </td>
                 {columns.map((column, colIndex) => (
