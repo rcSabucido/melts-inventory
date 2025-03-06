1e3501e9423c80a49edac5f1331b8ca52e702090
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-03-06 14:03:00 +0800

    Add ability to change ModifiableTable style

--- src/components/ModifiableTable.jsx
+++ src/components/ModifiableTable.jsx
@@ -1,14 +1,13 @@
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 
-const ModifiableTable = ({ columns, data }) => {
+const ModifiableTable = ({ columns, data, className }) => {
+    let addClassName = className;
     return (
-      <div className="relative overflow-x-auto rounded-xl m-5 shadow-[-4px_4px_4px_#888888]">
+      <div className={`relative overflow-x-auto rounded-xl m-5 ${addClassName}`}>
         <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-orange-200/70">
+          <thead className="text-xs text-gray-700 uppercase bg-[#ffe1b7]">
             <tr>
               <th className="px-3"></th>
-              <th className="px-6"></th>
-              <th className="px-6"></th>
               {columns.map((column, index) => (
                 <th key={index} scope="col" className="px-6 py-2">{column}</th>
               ))}
@@ -16,10 +15,13 @@ const ModifiableTable = ({ columns, data }) => {
           </thead>
           <tbody>
             {data.map((row, rowIndex) => (
-              <tr key={rowIndex} className="bg-amber-100 border-b border-gray-200 text-gray-900">
-                <td key="0" className="px-2 py-5"></td>
-                <td key="0" className="px-2 py-5 max-w-md"><PencilIcon /></td>
-                <td key="0" className="px-2 py-5 max-w-md"><TrashIcon /></td>
+              <tr key={rowIndex} className="bg-[#fff2bf] border-b border-gray-200 text-gray-900">
+                <td key="0" className="w-25 px-5">
+                  <div className='flex gap-8 justify-center items-center'>
+                    <PencilIcon className='h-5 w-5'/>
+                    <TrashIcon className='h-5 w-5'/>
+                  </div>
+                </td>
                 {columns.map((column, colIndex) => (
                   <td key={colIndex} className="px-6 py-5">{row[column]}</td>
                 ))}
