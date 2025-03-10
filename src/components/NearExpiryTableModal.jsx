c4d05bbec1736eb9996706d99a3511990a132056
LegionRevenant <danbap300@gmail.com>
2025-03-10 22:51:05 +0800

    #7 add pagination to near expiry

--- src/components/NearExpiryTableModal.jsx
+++ src/components/NearExpiryTableModal.jsx
@@ -1,7 +1,29 @@
 import NearExpiryTable from "../components/NearExpiry.jsx";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const NearExpiryTableModal = ({columns, data, onClose}) => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const rowsPerPage = 5;
+  
+    const indexOfLastRow = currentPage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
+  
+    const totalPages = Math.ceil(data.length / rowsPerPage);
+    const handleNextPage = () => {
+      if (currentPage  < totalPages) {
+        setCurrentPage(currentPage + 1);
+      }
+    };
+  
+    const handlePreviousPage = () => {
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      } 
+    };
+
     return (
         <div className="fixed inset-0 bg-gray-800/40 flex justify-center items-center z-50">
             <div className="bg-[#FDEFB2] p-6 rounded-lg w-3/4 h-3/4 overflow-auto">
@@ -9,8 +31,14 @@ const NearExpiryTableModal = ({columns, data, onClose}) => {
                     <h2 className="text-2xl font-bold">Near Expiry</h2>
                     <XMarkIcon className="h-6 w-6 cursor-pointer" onClick={onClose}/>
                 </div>
-                <NearExpiryTable columns={columns} data={data} />
+                <NearExpiryTable columns={columns} data={currentRows} />
             </div>
+            <div className='flex justify-center gap-4 absolute bottom-40 left-0 right-0'>
+            <button className='text-orange-500 hover:text-orange-700 font-medium' onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+            <button className='text-orange-500 hover:text-orange-700 font-medium' onClick={handleNextPage} disabled={currentPage === totalPages}>
+              Next {currentPage < totalPages && '→'}
+              </button>
+          </div>
         </div>
     )
 }
