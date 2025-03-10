6bb6c750eaef082705402139dec9c32c490f7652
LegionRevenant <danbap300@gmail.com>
2025-03-10 22:36:21 +0800

    #7 replace svg icon with heroicon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const SearchBar = () => {
     return (
@@ -10,9 +11,7 @@ const SearchBar = () => {
         className="h-9 absolute top-1 right-1 flex items-center rounded bg-orange-400 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-orange-400  focus:shadow-none active:hover:bg-orange-400/90 hover:hover:bg-orange-400/90 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mr-2">
-        <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
-        </svg>
+        <MagnifyingGlassIcon className="w-4 h-4 mr-2" />
         Search
         </button> 
         </div>
