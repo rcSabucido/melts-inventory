340a06590dec40a46ccf51af795ce3e5b2c80712
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-03-10 13:21:16 +0800

    #43 Rename class to className in some component files

--- src/components/NearExpiry.jsx
+++ src/components/NearExpiry.jsx
@@ -1,45 +1,45 @@
 const NearExpiry  = () => {
     return (
-      <div class="border-solid border-orange-400/40 border-2 rounded-md w-full">
-      <table class="w-full text-sm text-left rtl:text-right text-black">
-        <thead class="text-xs text-gray-700 uppercase bg-[#ffe1b7]">
+      <div className="border-solid border-orange-400/40 border-2 rounded-md w-full">
+      <table className="w-full text-sm text-left rtl:text-right text-black">
+        <thead className="text-xs text-gray-700 uppercase bg-[#ffe1b7]">
           <tr>
-            <th class="px-6 py-3">Product</th>
-            <th class="px-6 py-3">Items</th>
-            <th class="px-6 py-3">Category</th>
-            <th class="px-6 py-3">Days Left</th>
+            <th className="px-6 py-3">Product</th>
+            <th className="px-6 py-3">Items</th>
+            <th className="px-6 py-3">Category</th>
+            <th className="px-6 py-3">Days Left</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
           </tr>
           <tr>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
           </tr>
           <tr>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Banana Split</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">12</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Banana Split</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">12</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
           </tr>
           <tr>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Lorem Liquorice</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">15</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Lorem Liquorice</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">15</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
           </tr>
           <tr>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Ipsum Candy</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">25</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
-            <td class="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Ipsum Candy</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">25</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">Mango Chip</td>
+            <td className="px-6 py-5 border-solid border-gray-400/20 border-t-2">59</td>
           </tr>
         </tbody>
       </table>
