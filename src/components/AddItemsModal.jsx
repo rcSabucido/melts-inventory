25d3d563fe56e8a6f4a0d015b20d71fd0bd8fe5b
rcSabucido <rcsabucido@gmail.com>
2025-03-11 14:31:42 +0800

    #28 Write skeletal code for insert query in Inventory page

--- src/components/AddItemsModal.jsx
+++ src/components/AddItemsModal.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
+import { createClient } from '@supabase/supabase-js';
 
 const AddItems = ({ onClose }) => { 
+    const supabase =  createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
     const [formData, setFormData] = useState({
         product: '',
         category: '',
         price: '', 
     });
+
+    const [categories, setCategories] = useState([]);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -21,6 +26,35 @@ const AddItems = ({ onClose }) => {
         console.log(formData);
         onClose(); 
     };
+
+    useEffect(() => {
+        async function fetchCategories() {
+            const { data, error } = await supabase
+                .from('ProductCategory')
+                .select();
+                setCategories(data);
+        }
+        fetchCategories();
+
+        async function addProductName() {
+            const { error } = await supabase
+            .from('Product')
+            .insert({
+                category_id: formData.category,
+                name: formData.product
+            })
+        }
+        addProductName();
+   
+        async function addPrice() {
+            const { error } = await supabase
+            .from('InventoryItem')
+            .insert({
+                price: formData.price
+            })
+        }
+        addPrice();
+    }, []);
 
     return createPortal(
         <div className="fixed inset-0 flex justify-center items-center bg-gray-800/40"> 
@@ -40,21 +74,26 @@ const AddItems = ({ onClose }) => {
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-700">Category</label>
-                        <input
-                            type="text"
+                        <select
                             name="category"
                             value={formData.category}
                             onChange={handleChange}
                             className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md"
                             required
-                        />
+                        >
+                            {
+                                categories?.map(category => {
+                                    return <option key={category.category_id} value={category.category_id}>{category.name}</option>
+                                })
+                            }
+                        </select>
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-700">Price</label>
                         <input
                             type="number"
-                            name="addedItems"
-                            value={formData.addedItems}
+                            name="price"
+                            value={formData.price}
                             onChange={handleChange}
                             className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md"
                             required
