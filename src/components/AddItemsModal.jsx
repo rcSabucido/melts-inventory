106a95a13bdc5c011a52a9d52580fd1d268b130b
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-13 11:56:21 +0800

    #28 Add hover colours and cursor pointers to inventory buttons

--- src/components/AddItemsModal.jsx
+++ src/components/AddItemsModal.jsx
@@ -150,8 +150,8 @@ const AddItems = ({ onClose, refreshData }) => {
                         />
                     </div>
                     <div className="flex justify-center mt-6 gap-2 w-full">
-                        <button type="button" onClick={onClose} className="bg-[#CB3F3F] text-white p-2 w-full rounded">Cancel</button>
-                        <button type="submit" className="bg-orange-400/70 text-white p-2 w-full rounded">Save</button>
+                        <button type="button" onClick={onClose} className="bg-[#CB3F3F] hover:bg-[#BA3030] cursor-pointer text-white p-2 w-full rounded">Cancel</button>
+                        <button type="submit" className="bg-orange-400/70 hover:bg-orange-400/90 cursor-pointer text-white p-2 w-full rounded">Save</button>
                     </div>
                 </form>
             </div>
--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -10,8 +10,8 @@ const DeleteModal = ({ onClose, onConfirm }) => {
                 <h2 className="text-2xl font-bold mb-4 text-center">Confirm Deletion</h2>
                 <p className="text-center">Are you sure you want to delete this item?</p>
                 <div className="flex justify-center mt-6 gap-2 w-full">
-                    <button onClick={onClose} className="bg-[#CB3F3F] text-white p-2 w-50 rounded">Cancel</button>
-                    <button onClick={onConfirm} className="bg-orange-400/70 text-white p-2 w-50 rounded">Delete</button>
+                    <button onClick={onClose} className="bg-[#CB3F3F] hover:bg-[#BA3030] cursor-pointer text-white p-2 w-50 rounded">Cancel</button>
+                    <button onClick={onConfirm} className="bg-orange-400/70 hover:bg-orange-400/90 cursor-pointer text-white p-2 w-50 rounded">Delete</button>
                 </div>
             </div>
         </div>,
