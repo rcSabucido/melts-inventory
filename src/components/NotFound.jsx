99e6b3c90956faeefda64ce7979846aa7997f6ae
rcSabucido <rcsabucido@gmail.com>
2025-03-14 00:30:49 +0800

    #47 Add NotFound page to App.jsx and route any invalid URLs to that page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,19 @@
 import { useNavigate } from "react-router-dom";
 
-const notFound = () => {
+const NotFound = () => {
     const navigate = useNavigate();
     return (
-        <div></div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#ffffdb]">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Page Not Found</h1>
+        <p className="text-gray-600 mb-8">The page you are looking for does not exist.</p>
+        <button 
+          onClick={() => navigate('/home')}
+          className="bg-orange-400/70 hover:bg-orange-400/90 text-white font-bold py-2 px-4 rounded cursor-pointer"
+        >
+          Return Home
+        </button>
+      </div>
     );
 }
 
-export default notFound;
+export default NotFound;
