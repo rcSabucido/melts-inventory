212a5b35747bdf114fe180971e77b1372a20db74
rcSabucido <rcsabucido@gmail.com>
2025-03-14 00:26:59 +0800

    #47 Create NotFound component to route invalid urls to this page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
+const notFound = () => {
+    const navigate = useNavigate();
+    return (
+        <div></div>
+    );
+}
+
+export default notFound;
