b511de270ef19fb0b2e4a6aa6bfd428cffef71af
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2025-03-13 15:38:14 +0800

    #33 Add styling to RestockDateGroup component

--- src/components/RestockDateGroup.jsx
+++ src/components/RestockDateGroup.jsx
@@ -1,6 +1,21 @@
+import RestockTable from "./RestockTable";
+import { ArrowsPointingOutIcon } from "@heroicons/react/24/solid";
+
 const RestockDateGroup = ({ date, data, columns, onExpand }) => {
+    const limitedData = data.slice(0, 4);
     return(
-        <></>
+        <div className='mx-7 w-auto pb-0.5 bg-amber-200/30 rounded-xl mb-4'>
+        <div className='flex justify-between'>
+          <p className='px-4 pt-4 text-xl font-bold'>{date}</p>
+          {data.length >= 4 && (
+            <ArrowsPointingOutIcon 
+              className='h-6 w-6 mr-6 mt-4 cursor-pointer' 
+              onClick={() => onExpand(date, data)}
+            />
+          )}
+        </div>
+        <RestockTable columns={columns} data={limitedData} />
+      </div>
     );
 }
 
