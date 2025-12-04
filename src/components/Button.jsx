39f7f06b58a1c72c40b0658b2a75f115febbe422
evilk1aus <124642134+evilk1aus@users.noreply.github.com>
2025-12-04 13:06:00 +0800

    Add ripple effect to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+
 const Button = ({ children, onClick, type }) => {
+    const [ripple, setRipple] = useState(null);
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const clickEvent = async (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const size = Math.max(rect.width, rect.height);
+        const x = e.clientX - rect.left - size / 2;
+        const y = e.clientY - rect.top - size / 2;
+
+        setRipple({ x, y, size });
+
+        setTimeout(() => setRipple(null), 600);
+        setTimeout(() => onClick(e), 250);
+    }
+
     return (
-        <button type={type} className="flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer" onClick={onClick}>
+        <button type={type} className="ripple-container flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer" onClick={clickEvent}>
             {children}
+            {ripple && (
+                <span
+                    className="ripple-effect"
+                    style={{
+                        width: ripple.size,
+                        height: ripple.size,
+                        left: ripple.x,
+                        top: ripple.y,
+                    }}
+                />
+            )}
         </button>
     );
 }
