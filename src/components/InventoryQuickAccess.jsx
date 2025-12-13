5fc43a3e82eee1620cb37bd6cfbb9d649a8b1185
evilk1aus <124642134+evilk1aus@users.noreply.github.com>
2025-12-14 04:49:59 +0800

    Create CategoryButton component

--- src/components/InventoryQuickAccess.jsx
+++ src/components/InventoryQuickAccess.jsx
@@ -1,8 +1,16 @@
 import { createPortal } from "react-dom";
+import { CakeIcon } from '@heroicons/react/20/solid';
 
 import Button from "../components/Button";
 
 const InventoryQuickAccess = () => {
+    const CategoryButton = ({text, children, onClick}) => {
+        return <>
+            <Button className="flex flex-col" onClick={onClick}>{text} {children}</Button>
+        </>;
+    }
+    const iconClassName = "fill-white w-10";
+
     return createPortal(
         <div className="fixed inset-0 flex w-3/4 h-7/8 m-auto border border-gray-300 rounded-xl shadow-xl"> 
             <main className="bg-[#ffffdb] w-full h-full rounded-l-xl">
@@ -11,7 +19,9 @@ const InventoryQuickAccess = () => {
 
             <aside className="h-full sticky top-0 bg-[#FFB64F] min-w-3xs overflow-y-auto overflow-x-hidden rounded-r-xl">
                 <div className="flex flex-col h-full p-10 justify-between items-center w-full">
-                    <Button className="">ALL</Button>
+                    <CategoryButton text="ALL">
+                        <CakeIcon className={iconClassName}/>
+                    </CategoryButton>
                     <Button>Cake</Button>
                     <Button>Drinks</Button>
                     <Button>Sweets</Button>
